19cbc6d8b250eb4f083abaa29e5c67ebde103197
Mahmoud Ismail <m.ismail@speero.net>
2021-08-19 14:50:01 +0200

    added several data for several charts

--- data.js
+++ data.js
@@ -14,7 +14,6 @@ const buildConfig = [
     priority: 1,
     styles: {
       width: '30vw',
-      height: '2vw',
       order: '2'
     },
     dataSource: '',
@@ -33,12 +32,11 @@ const buildConfig = [
     }
   },
   {
-    id: '1',
+    id: '2',
     position: '',
     priority: 1,
     styles: {
-      width: '30vw',
-      height: '2vw',
+      width: '20vw',
       order: '2'
     },
     dataSource: '',
@@ -56,5 +54,97 @@ const buildConfig = [
       },
     }
   },
+  {
+    id: '3',
+    position: '',
+    priority: 1,
+    styles: {
+      width: '20vw',
+      order: '3'
+    },
+    dataSource: '',
+    config: {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'My Third Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'blue',
+          tension: 0.1
+        }]
+      },
+    }
+  },
+  {
+    id: '4',
+    position: '',
+    priority: 1,
+    styles: {
+      width: '20vw',
+      order: '4'
+    },
+    dataSource: '',
+    config: {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'My fourth Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'green',
+          tension: 0.1
+        }]
+      },
+    }
+  },
+  {
+    id: '5',
+    position: '',
+    priority: 1,
+    styles: {
+      width: '20vw',
+      order: '5'
+    },
+    dataSource: '',
+    config: {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'My fifth Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'orange',
+          tension: 0.1
+        }]
+      },
+    }
+  },
+  {
+    id: '6',
+    position: '',
+    priority: 1,
+    styles: {
+      width: '20vw',
+      order: '1'
+    },
+    dataSource: '',
+    config: {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'My sixth Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'black',
+          tension: 0.1
+        }]
+      },
+    }
+  }
 ]
 
