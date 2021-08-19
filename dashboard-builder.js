7b5f45e60e133b77ee7090d99c3b39cf9f8b0310
Mahmoud Ismail <m.ismail@speero.net>
2021-08-19 17:38:41 +0200

    Use fetch API to get json data and feed it to builder method

--- dashboard-builder.js
+++ dashboard-builder.js
@@ -1,20 +1,26 @@
 
-  (function (buildConfig) {
-    const container = document.getElementById('container');
-    for(let i = 0; i < buildConfig.length; i++){
-      const bc = buildConfig[i];
-          generateChart(bc)
-    }
-    function generateChart(config){
-      const chartContainer = document.createElement('div');
-      for(key in config.styles){
-        chartContainer.style[key] = config.styles[key];
-      }
-      const canvas = document.createElement('canvas');
-      canvas.id = config.id
-      container.appendChild(chartContainer).appendChild(canvas)
-      const ctx = canvas.getContext('2d');
-      const chart = new Chart(ctx, config.config);
-    }
-  })(buildConfig)
+fetch('data.json').then(res => res.json()).then(res => {
+  const buildConfig = res.buildConfig
+  builder(buildConfig)
+})
+
+function generateChart(config) {
+  const chartContainer = document.createElement('div');
+  for (key in config.styles) {
+    chartContainer.style[key] = config.styles[key];
+  }
+  const canvas = document.createElement('canvas');
+  canvas.id = config.id
+  container.appendChild(chartContainer).appendChild(canvas)
+  const ctx = canvas.getContext('2d');
+  const chart = new Chart(ctx, config.config);
+}
+
+function builder(buildConfig) {
+  const container = document.getElementById('container');
+  for (let i = 0; i < buildConfig.length; i++) {
+    const bc = buildConfig[i];
+    generateChart(bc)
+  }
+}
 
