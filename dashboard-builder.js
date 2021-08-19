48b37fb64952fe193d22f60ec9dd2f49de01b248
Mahmoud Ismail <m.ismail@speero.net>
2021-08-19 13:27:38 +0200

    implement generateChart method

--- dashboard-builder.js
+++ dashboard-builder.js
@@ -1,10 +1,20 @@
 
-  (function (data) {
-    const config = {
-      type: 'line',
-      data: data,
-      options: {}
-    };
-    const myChart = new Chart(document.getElementById('myChart'), config)
-  })(data)
+  (function (buildConfig) {
+    const container = document.getElementById('container');
+    for(let i = 0; i < buildConfig.length; i++){
+      const bc = buildConfig[i];
+          generateChart(bc)
+    }
+    function generateChart(config){
+      const chartContainer = document.createElement('div');
+      for(key in config.styles){
+        chartContainer.style[key] = config.styles[key];
+      }
+      const canvas = document.createElement('canvas');
+      canvas.id = config.id
+      container.appendChild(chartContainer).appendChild(canvas)
+      const ctx = canvas.getContext('2d');
+      const chart = new Chart(ctx, config.config);
+    }
+  })(buildConfig)
 
