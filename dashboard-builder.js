bddc492a086fedea18991fc2a98b4be11873bf67
Mahmoud Ismail <m.ismail@speero.net>
2021-08-19 23:45:57 +0200

    Enabled fetching data from JSON file as per priority

--- dashboard-builder.js
+++ dashboard-builder.js
@@ -1,26 +1,46 @@
+const container = document.getElementById('container');
 
-fetch('data.json').then(res => res.json()).then(res => {
-  const buildConfig = res.buildConfig
-  builder(buildConfig)
-})
+fetch('data.json')
+  .then(res => res.json())
+  .then(res => {
+    const buildConfig = res.buildConfig;
+    builder(buildConfig)
+  })
 
-function generateChart(config) {
+function generateChart(setup) {
   const chartContainer = document.createElement('div');
-  for (key in config.styles) {
-    chartContainer.style[key] = config.styles[key];
+  for (key in setup.styles) {
+    chartContainer.style[key] = setup.styles[key];
   }
   const canvas = document.createElement('canvas');
-  canvas.id = config.id
-  container.appendChild(chartContainer).appendChild(canvas)
+  canvas.id = setup.id;
+  container.appendChild(chartContainer).appendChild(canvas);
   const ctx = canvas.getContext('2d');
-  const chart = new Chart(ctx, config.config);
+  const chart = new Chart(ctx, setup.config);
 }
 
 function builder(buildConfig) {
-  const container = document.getElementById('container');
   for (let i = 0; i < buildConfig.length; i++) {
     const bc = buildConfig[i];
-    generateChart(bc)
+    if (!bc.dataSourceURL) {
+      generateChart(bc);
+    }
   }
+  const priorityConfig = buildConfig.filter(bc => bc.dataSourceURL).sort((a, b) => a.priority - b.priority);
+  displayPriorityCharts(priorityConfig, 0);
+}
+
+function displayPriorityCharts(config, i) {
+  if (!config.length) return;
+  fetch(config[i].dataSourceURL)
+    .then(res => res.json())
+    .then(res => {
+      config[i].config.data = res.data
+      generateChart(config[i]);
+      if (config[i + 1]) {
+        i++;
+        displayPriorityCharts(config, i);
+      }
+    })
 }
 
