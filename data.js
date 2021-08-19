71928299d8a2e0009af4a7dbcfaa7f509fa80f51
Mahmoud Ismail <m.ismail@speero.net>
2021-08-19 13:26:57 +0200

    construct chart data

--- data.js
+++ data.js
@@ -6,12 +6,55 @@ const labels = [
   'May',
   'June',
 ];
-const data = {
-  labels: labels,
-  datasets: [{
-    label: 'My First dataset',
-    backgroundColor: 'rgb(255, 99, 132)',
-    borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 20, 30, 45],
-  }]
-};
+
+const buildConfig = [
+  {
+    id: '1',
+    position: '',
+    priority: 1,
+    styles: {
+      width: '30vw',
+      height: '2vw',
+      order: '2'
+    },
+    dataSource: '',
+    config: {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+      },
+    }
+  },
+  {
+    id: '1',
+    position: '',
+    priority: 1,
+    styles: {
+      width: '30vw',
+      height: '2vw',
+      order: '2'
+    },
+    dataSource: '',
+    config: {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'My Second Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'red',
+          tension: 0.1
+        }]
+      },
+    }
+  },
+]
+
